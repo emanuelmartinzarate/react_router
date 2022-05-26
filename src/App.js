cca75f688f9493efa6aec3a8a45e6d05f082d711
Emanuel Zarate <emanuelmartin@gmail.com>
2022-05-26 18:02:42 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import Contacto from "./components/Contacto";
+import Inicio from "./components/Inicio";
+import Nosotros from "./components/Nosotros";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+          <div className="btn-group">
+            <Link to="/" className="btn btn-dark mt-5">
+              Inicio
+            </Link>
+            <Link to="/nosotros" className="btn btn-dark mt-5">
+              Nosotros
+            </Link>
+            <NavLink to="/contacto" className="btn btn-dark mt-5" activeClassName="active">
+              Contacto
+            </NavLink>
+          </div>
+          <hr />
+          <Switch>
+            <Route path="/nosotros/:id">
+              <User />
+            </Route>
+            <Route path="/" exact>
+              <Inicio />
+            </Route>
+            <Route path="/contacto">
+              <Contacto />
+            </Route>
+            <Route path="/nosotros">
+              <Nosotros />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
